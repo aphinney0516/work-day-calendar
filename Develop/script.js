064734dbe2897f022bf600abafb983c36af3af2b
Andy Phinney <111609415+aphinney0516@users.noreply.github.com>
2022-10-30 13:48:46 -0500

    Attempted to code the app from scratched and initially failed miserably, per my usual

---
+++ Develop/script.js
@@ -0,0 +1,9 @@
+//function to update the present block of time
+
+//function to save user input into a block of time
+
+//function to change past block to gray and future blocks to grteen
+
+//bonus function to change future booked appointment to yellow
+
+//if there's time make a function to notify when events are saved
